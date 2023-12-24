f71cb2ba294f1710fe68f770ac71553522791bcc
Harshal Khond <86043419+harshalkhond@users.noreply.github.com>
2023-12-24 12:39:20 +0530

    Update Chapters.jsx

--- src/Components/Chapters.jsx
+++ src/Components/Chapters.jsx
@@ -22,7 +22,7 @@ export const Chapters = () => {
                 method: 'GET',
                 url: url,
                 headers: {
-                    'X-RapidAPI-Key': 'ede96b6ee0msh01977ae033ea98cp120734jsna9da51ef0d12',
+                    'X-RapidAPI-Key': 'apikey',
                     'X-RapidAPI-Host': 'bhagavad-gita3.p.rapidapi.com'
                 }
             };
